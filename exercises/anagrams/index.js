9b5928c53612bd01c4dba095240f0ab4ba1f2333
Yvette <yvettetran@gmail.com>
2018-08-19 17:42:29 -0700

    added 2nd solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,16 +8,44 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
-	const strA = stringA.toLowerCase().replace(/[^\w]/g, "").split("").sort().join("");
-	const strB = stringB.toLowerCase().replace(/[^\w]/g, "").split("").sort().join("");
+// function anagrams(stringA, stringB) {
+// 	const strA = stringA.toLowerCase().replace(/[^\w]/g, "").split("").sort().join("");
+// 	const strB = stringB.toLowerCase().replace(/[^\w]/g, "").split("").sort().join("");
 
-	if(strA === strB) {
-		return true;
-	}
-	else {
+// 	if(strA === strB) {
+// 		return true;
+// 	}
+// 	else {
+// 		return false;
+// 	}
+// }
+
+function anagrams(stringA, stringB) {
+	const charMapA = charMap(stringA);
+	const charMapB = charMap(stringB);
+
+	if(Object.keys(charMapA).length !== Object.keys(charMapB).length) {
 		return false;
 	}
+
+	for(let char in charMapA) {
+		if(charMapA[char] !== charMapB[char]) {
+			return false;
+		}
+	}
+
+	return true;
 }
+
+function charMap(str) {
+	const charMap = {};
+
+	for(let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+		charMap[char] = charMap + 1 || 1;
+	}
+
+	return charMap;
+}
+
 
 module.exports = anagrams;
